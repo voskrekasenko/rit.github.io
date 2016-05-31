dd09b8e97fcac7b51111f13b8284f8f8abe69ead
Max Voskrekasenko <voskrekasenko@gmail.com>
2016-05-31 15:29:30 +0300

    blured

--- js/pages/navController.js
+++ js/pages/navController.js
@@ -1,1 +1,1 @@
-!function(){"use strict";function e(){var e=document.querySelector(".hamburger");e.addEventListener("click",function(){e.classList.toggle("is-active")})}angular.module("app").controller("navController",e),e.$inject=["$scope","$state","$rootScope"]}();
+!function(){"use strict";function e(e){var t=document.querySelector(".hamburger");t.addEventListener("click",function(){t.classList.toggle("is-active")}),e.blured=!1,e.toggleBlur=function(){e.blured=!e.blured}}angular.module("app").controller("navController",e),e.$inject=["$scope","$state","$rootScope"]}();
